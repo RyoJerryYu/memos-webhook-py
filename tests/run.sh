fa7925bc415f5677847f688cc4110398ff337f61
Ryo <36154873+RyoJerryYu@users.noreply.github.com>
2024-06-13 02:50:24 +0800

    feat: ignore docker compose file in git

--- tests/run.sh
+++ tests/run.sh
@@ -1,1 +1,5 @@
-docker-compose -f tests/docker_compose.yaml up -d
+if [ ! -f tests/docker_compose.yaml ]; then
+    cp tests/docker_compose.example.yaml tests/docker_compose.yaml
+fi
+
+docker-compose -f tests/docker_compose.yaml up -d --no-deps --build
